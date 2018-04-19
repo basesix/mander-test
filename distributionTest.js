b048dfc5692b3979ffaa655310e8641a419e5779
basesix <ckersh88@gmail.com>
2018-04-19 07:42:50 -0700

    Basic UI with very basic results

--- distributionTest.js
+++ distributionTest.js
@@ -8,18 +8,22 @@ function generateDistribution(margin, nDistricts){
 	
 	var s = 101;
 	
-	var nA = (s-margin)/2*nDistricts;
+	var nA = (s+margin)/2*nDistricts;
 		
 	var n = s*nDistricts;
 	var districts = [];
 	
 	var r, i, newDistrict;
+	var districtNumber = 1;
 	
 	while(n > 0){
 		newDistrict = {
 			a : 0,
-			b : 0
+			b : 0,
+			number: +districtNumber
 		};
+		
+		districtNumber++;
 		
 		for(i = 0; i < s; i++){
 			r = getRandInt(n);
@@ -33,20 +37,25 @@ function generateDistribution(margin, nDistricts){
 			n--;
 		}
 		
+		newDistrict.winner = (newDistrict.a > newDistrict.b) ? 'a' :
+			(newDistrict.b > newDistrict.a) ? 'b' : 'TIE';
+		
 		districts.push(newDistrict);
 	}
 	
 	return districts;
 }
 
-function getDistributionStats = function(dist){
+function getDistributionStats(dist){
 	var stats = {
 		voteResults : {
 			winsA : 0,
 			winsApct : 0,
 			winsB : 0,
 			winsBpct : 0,
-			totalWins : 0
+			ties : 0,
+			tiesPct : 0,
+			totalDists : 0
 		},
 		general : {
 			
@@ -56,13 +65,20 @@ function getDistributionStats = function(dist){
 	dist.forEach(function(thisDist){
 		if(thisDist.a > thisDist.b){
 			stats.voteResults.winsA += 1;
-			
+		} else if (thisDist.b > thisDist.a){
+			stats.voteResults.winsB += 1;
 		} else {
-			
+			stats.voteResults.ties += 1;
 		}
 		
-		stats.voteResults.totalWins += 1;
+		stats.voteResults.totalDists += 1;
 	});
+	
+	var voteResults = stats.voteResults;
+	
+	voteResults.winsApct = voteResults.winsA / voteResults.totalDists;
+	voteResults.winsBpct = voteResults.winsB / voteResults.totalDists;
+	voteResults.tiesPct = voteResults.ties / voteResults.totalDists;
 	
 	return stats;
 };
